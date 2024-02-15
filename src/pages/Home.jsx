bea0e2e9c0ad9df4a8368cb0977b3b1671b313eb
rizwankhan <super047240@gmail.com>
2024-02-15 11:45:23 +0530

    integrate frontend with backend

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-black w-screen h-screen'>
+      <h1 className=' text-white'>welcome to homepage</h1>
+    </div>
+  )
+}
+
+export default Home
