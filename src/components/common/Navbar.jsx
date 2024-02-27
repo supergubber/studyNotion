158c001dd81fd11b2f264220e18c44676cf123aa
rizwankhan <super047240@gmail.com>
2024-02-27 16:38:19 +0530

    navigation bar completer

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -57,7 +57,22 @@ const Navbar = () => {
                   <div className='relative flex flex-row items-center gap-2 cursor-pointer group'>
                     <p>{link.title}</p>
                     <MdOutlineKeyboardArrowDown className='w-5 h-5' />
-                    <div className='invisible absolute left-[50%] top-[50%] flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[300px]'></div>
+                    <div className='invisible absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[30%] z-10 flex flex-col rounded-md bg-richblack-5 py-2 text-richblack-900 opacity-0 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[200px]'>
+                      <div className='absolute left-[50%] top-0 translate-x-[80%] translate-y-[-45%] h-6 w-6 rotate-45 rounded bg-richblack-5'></div>
+                      {subLinks.length ? (
+                        subLinks.map((subLink, index) => (
+                          <Link
+                            to={`${subLink.links}`}
+                            key={index}
+                            className='z-10 px-4 py-2 hover:bg-richblack-50'
+                          >
+                            <p>{subLink.title}</p>
+                          </Link>
+                        ))
+                      ) : (
+                        <div></div>
+                      )}
+                    </div>
                   </div>
                 ) : (
                   <Link to={link?.path}>
